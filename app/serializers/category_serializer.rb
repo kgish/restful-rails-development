fe1b322028e1f42c5c0f2dbf98bb496f986dd5e3
Kiffin Gish <kiffin.gish@planet.nl>
2015-07-22 22:33:10 +0200

    Category serializer strips cat_ from attribute names for more readable json.

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,10 @@
 class CategorySerializer < ActiveModel::Serializer
-  #attributes :id
-  attributes :id, :title, :sub_categories
+  attributes :title, :sub_categories
+  def title
+    URI::encode(object.cat_title.force_encoding('ISO-8859-1').encode('utf-8', replace: nil).downcase.tr(' ', '_'))
+  end
+
+  def sub_categories
+    object.cat_subcats
+  end
 end
